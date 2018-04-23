390516db84a0d957eb01a7f3cfc18b6851b3a64e
unknown <zumaconzeta@gmail.com>
2018-04-23 20:21:10 -0300

    commit 2

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ var UserSchema = Schema({
 //,{ collection: NombreModelo }
                         // Esto es una prueba de Git
 
-// esto es una prueba de vuelta 
+// esto es una prueba de vuelta 2
 );
 
 module.exports = mongoose.model('User',UserSchema);
