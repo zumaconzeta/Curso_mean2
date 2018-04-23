d3f7e34095adecae337e6d1016b52b2e0832dabf
zumaconzeta <zumaconzeta@gmail.com>
2018-04-23 18:33:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//cargas rutas
+var user_routes = require('./routes/user');
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//configurar cabeceras http
+
+//ruta base
+
+// esto es para agregar la /api
+app.use('/api', user_routes);
+
+/* app.get('/pruebas',function(req, res){
+    res.status(200).send({message: 'bienvenido'}) ;
+});*/
+
+module.exports = app;
