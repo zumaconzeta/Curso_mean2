285b57f4051d33a8764d04909f1fda226bd1a16c
zumaconzeta <zumaconzeta@gmail.com>
2018-04-23 19:42:39 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,6 +14,7 @@ var UserSchema = Schema({
     image: String
 } 
 //,{ collection: NombreModelo }
+                        // Esto es una prueba de Git
 );
 
 module.exports = mongoose.model('User',UserSchema);
