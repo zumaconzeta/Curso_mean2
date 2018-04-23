47e38c08eda5453659e0ec0099017f7a9c1c2b49
zumaconzeta <zumaconzeta@gmail.com>
2018-04-23 18:30:27 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//var NombreModelo = 'User';
+
+var mongoose =require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    name: String,
+    surname: String,
+    email: String,
+    password: String,
+    role: String,
+    image: String
+}, 
+//{ collection: NombreModelo }
+);
+
+module.exports = mongoose.model('User',UserSchema);
