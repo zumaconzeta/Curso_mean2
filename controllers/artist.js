06ccd755d7bc136efca5a9032fda7e4884269d59
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-04-25 18:09:17 -0300

    work Artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -10,6 +10,29 @@ function getArtist(req, res){
     res.status(200).send({message: 'Metodo getArtist del controlado artist.js'});
 }
 
+function saveArtist (req,res){
+    var artist = new Artist();
+
+    var params = req.body;
+    artist.name = params.name;
+    artist.description = params.description;
+    artist.image= 'null';
+
+
+    artist.save((err,artistSored) =>{
+        if (err){
+            res.status(500).send({message:'Error al guardar el artista'});
+        }else{
+            if(!artistSored){
+                res.status(404).send({message:'El artista no ha sido giuardado'});
+            }else{
+                res.status(200).send({artist: artistSored});
+            }
+        }
+    });
+}
+
 module.exports ={
-        getArtist
+        getArtist,
+        saveArtist
 };
