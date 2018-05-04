1faca0ccaa2f76d3a97a691c04c6713feac40432
zumaconzeta <zumaconzeta@gmail.com>
2018-05-04 00:28:54 -0300

    song

--- routes/song.js
+++ routes/song.js
@@ -9,6 +9,7 @@ var md_auth = require('../middlewares/authenticated');
 var multipart = require('connect-multiparty');
 var md_upload = multipart({uploadDir:'./uploads/song'});
 
-api.get('/song', songController.getSong);
+api.get('/song',md_auth.ensureAuth, songController.getSong);
+api.post('/song',md_auth.ensureAuth, songController.saveSong);
 
 module.exports= api;
