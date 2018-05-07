e7867ce96e7475b64e23b2adc634bc44529de349
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-05-07 13:44:36 -0300

    Archivo de audio

--- controllers/song.js
+++ controllers/song.js
@@ -26,8 +26,29 @@ function getSong(req,res){
 
 
 function getSongs(req,res){
-    var albumID = req.params.album;
-    
+    var albumId = req.params.album;
+    if(!albumId){
+        var find = Song.find({}).sort('number')
+    }else{
+        var find = Song.find({album: albumId}).sort('number');
+    }
+    find.populate({
+        path: 'album',
+        populate:{
+            path: 'artist',
+            model: 'Artist'
+        }
+    }).exec(function(err,songs){
+        if(err){
+            res.status(500).send({message: 'Error en el servidor '});
+        }else{
+            if (!songs){
+                res.status(404).send({message: 'No existe las canciones'});   
+            }else{
+                res.status(200).send({songs});
+            }
+        }
+    });
 }
 
 function saveSong (req,res){
@@ -51,8 +72,92 @@ function saveSong (req,res){
     });
 }
 
+function updateSong (req,res){
+    var songId = req.params.id;
+    var update = req.body;
+
+    Song.findByIdAndRemove(songId, update,(err,songRemoved)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});            
+        }else{
+            if(!songRemoved){                
+                res.status(404).send({message: 'No se ha actualizado la canción '});
+            }else{                
+                res.status(200).send({song: songRemoved});
+            }
+        }
+    });
+
+}
+
+function deleteSong(req,res){
+    var songId = req.params.id
+    Song.findByIdAndRemove(songId,(err,songRemoved)=>{
+        if (err){
+            res.status(500).send({message: 'Error al eliminar Canción'});
+        }else{
+            if(!songRemoved){
+                res.status(404).send({message: 'La canción no ha sido eliminado'}); 
+            }else{
+                res.status(200).send({song:songRemoved});
+            }
+
+        }
+    });
+}
+
+function UploadFile (req,res){
+    var songId = req.params.id;
+    var file_name = 'No Subido...';
+    if (req.files){
+        var file_path = req.files.file.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'mp3' || file_ext == 'ogg'){
+            Song.findByIdAndUpdate(songId, {file: file_name},(err,songUpdated)=>{
+                if (!songUpdated){
+                    res.status(404).send({message: 'no se ha actualizado la cancion'});
+                }else{
+                    res.status(200).send({song: songUpdated});
+                }
+            });
+        }else{
+            res.status(200).send({message: 'Tipo de archivo no valido'});
+        }
+
+        console.log (file_split);
+    }else{
+        res.status(200).send({message: 'El audio no se ha subido'});
+    }
+
+}
+
+function getSongFile(req,res){
+    
+    var songFile = req.params.songFile;
+    var path_file = './uploads/song/' + songFile;
+
+    fs.exists(path_file, function(exists){
+        if(exists){
+            res.sendFile(path.resolve(path_file));
+        }else{
+            res.status(200).send({message: 'No existe el audio'});
+        }
+
+    });
+}
+
 
 module.exports = {
     getSong
     ,saveSong
+    ,getSongs
+    ,updateSong
+    ,deleteSong
+    ,UploadFile
+    ,getSongFile
 }
--- routes/song.js
+++ routes/song.js
@@ -11,5 +11,9 @@ var md_upload = multipart({uploadDir:'./uploads/song'});
 
 api.get('/song/:id',md_auth.ensureAuth, songController.getSong);
 api.post('/song',md_auth.ensureAuth, songController.saveSong);
-
+api.get('/songs/:album?',md_auth.ensureAuth, songController.getSongs);
+api.put('/song/:id',md_auth.ensureAuth, songController.updateSong);
+api.delete('/song/:id',md_auth.ensureAuth, songController.deleteSong);
+api.post('/upload-file-song/:id',[md_auth.ensureAuth,md_upload],songController.UploadFile);
+api.get('/get-file-song/:songFile',songController.getSongFile);
 module.exports= api;
