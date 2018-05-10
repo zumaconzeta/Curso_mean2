30e01422264e9680ac72467ce580b690e0d8a090
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-05-10 17:56:02 -0300

    Coneccion a Atlas

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
+var atlas = "mongodb://admin:admin@cluster0-shard-00-00-euqmy.mongodb.net:27017,cluster0-shard-00-01-euqmy.mongodb.net:27017,cluster0-shard-00-02-euqmy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var local = 'mongodb://localhost:27017/curso_mean2';
 
-//mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/curso_mean2', (err,res) => {
+ //mongoose.Promise = global.Promise;
+mongoose.connect(atlas, (err,res) => {
     if(err){
         //throw err;
         console.log('problema con la conexion con MongoDB');
@@ -17,3 +19,6 @@ mongoose.connect('mongodb://localhost:27017/curso_mean2', (err,res) => {
         });
     }
 });
+ 
+
+
