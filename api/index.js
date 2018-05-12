0fe621525d7b747532ebf3f41081c13f00011b35
zumaconzeta <zumaconzeta@gmail.com>
2018-05-11 21:34:04 -0300

    cambio coneccion Nombre BD

--- api/index.js
+++ api/index.js
@@ -3,11 +3,11 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
-var atlas = "mongodb://admin:admin@cluster0-shard-00-00-euqmy.mongodb.net:27017,cluster0-shard-00-01-euqmy.mongodb.net:27017,cluster0-shard-00-02-euqmy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var atlas = "mongodb://admin:admin@cluster0-shard-00-00-euqmy.mongodb.net:27017,cluster0-shard-00-01-euqmy.mongodb.net:27017,cluster0-shard-00-02-euqmy.mongodb.net:27017/curso_mean2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 var local = 'mongodb://localhost:27017/curso_mean2';
 
  //mongoose.Promise = global.Promise;
-mongoose.connect(local, (err,res) => {
+mongoose.connect(atlas, (err,res) => {
     if(err){
         //throw err;
         console.log('problema con la conexion con MongoDB');
