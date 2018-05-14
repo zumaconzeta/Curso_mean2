f76cbf7ffe338b6518c80436946edcd3f034eb73
zumaconzeta <zumaconzeta@gmail.com>
2018-05-14 01:53:51 -0400

    Login Form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import {User} from './models/user'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  title = 'MUSIFY';
+ public  title = 'MUSIFY';
+ public user: User;
+ public identity;
+ public token;
+
+ constructor(){
+  this.user = new User('','','','','','ROLE_USER','');
+ }
+
 }
