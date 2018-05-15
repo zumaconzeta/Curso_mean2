0cec8a1b814818e3c0ea9408b60faa42608937b8
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-05-15 19:05:44 -0400

    login y registro FrontEnd

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import {UserService} from './services/user.service';
 import {User} from './models/user'
+import { Response } from '@angular/http';
 
 
 @Component({
@@ -12,26 +13,35 @@ import {User} from './models/user'
 export class AppComponent {
  public  title = 'MUSIFY';
  public user: User;
+ public user_register: User;
  public identity;
  public token;
  public errorMessage;
+ public alertRegister;
 
  constructor(
    private _userService:UserService
  ){
   this.user = new User('','','','','','ROLE_USER','');
+  this.user_register = new User('','','','','','ROLE_USER','');
  }
 
 ngOnInit(){
  //var texto = this._userService.signup();
  //console.log(texto);
+  this.identity= this._userService.getIdentity();
+  this.token= this._userService.getToken();
+
+  console.log(this.identity);
+  console.log(this.token);
+
 }
 
 public onSubmit(){
   console.log(this.user);
 
 
-  // consegui rlos datosa del usuairo
+  // conseguir los datos del usuairo
   this._userService.signup(this.user).subscribe(
     response => {
       let identity = response.user;
@@ -41,7 +51,8 @@ public onSubmit(){
         alert("El usuario no está correctamente Identificado");
       }else{
         // crear elemento en el localstorage para tener el usuario sesión
-
+        localStorage.setItem('identity',JSON.stringify(identity));
+                
         // conseguir el Token para enviarselo a cada petición http
         this._userService.signup(this.user,'true').subscribe(
           response => {
@@ -51,9 +62,9 @@ public onSubmit(){
             if(this.token.length <= 0 ){
               alert("El token no se ha generado");
             }else{
-              // crear elemento en el localstorage para tener Token disponible      
-              console.log(this.token);
-              console.log(identity);              
+              // crear elemento en el localstorage para tener Token disponible    
+              localStorage.setItem('token',token);
+              this.user = new User('','','','','','ROLE_USER','');   
             }
           },
           error => {
@@ -63,8 +74,7 @@ public onSubmit(){
               this.errorMessage = body.message;
               console.log(error);
             }
-          }
-          
+          }          
         );
       }
     },
@@ -75,9 +85,46 @@ public onSubmit(){
         this.errorMessage = body.message;
         console.log(error);
       }
-    }
-    
+    }    
   );
+}
+
+
+
+onSubmitRegister(){
+  console.log(this.user_register);
+  this._userService.register(this.user_register).subscribe( 
+    response => {
+      let user = response.user;
+      this.user_register = user;
+
+      if(!user._id){
+        this.alertRegister ='Error al registrarse';
+      }else{
+        this.alertRegister = 'Registro Correcto, identificate con ' + this.user_register.email;
+        this.user_register = new User('','','','','','ROLE_USER','');
+      }
+
+  },
+  error => {
+    var errorMessage = <any> error;
+    if(errorMessage != null){
+      var body = JSON.parse(error._body);
+      this.alertRegister = body.message;
+      console.log(error);
+    }
+  } 
+  
+
+  );
+}
+
+logout(){
+ localStorage.removeItem('identity');
+ localStorage.removeItem('token');
+ localStorage.clear() ;
+ this.identity = null;
+ this.token = null;
 }
 
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -6,10 +6,13 @@ import { map } from 'rxjs/operators';
 //import { Observable } from 'rxjs';
 import { isPending } from 'q';
 import {GLOBAL} from './global';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 
 export class UserService{
+    public identity;
+    public token;
     public url: string;
 
     constructor(private _http: Http){
@@ -22,7 +25,6 @@ export class UserService{
         if (gethash != null){
             user_to_login.gethash = gethash;
         }
-
         let json = JSON.stringify(user_to_login)
         let params = json;
 
@@ -31,4 +33,34 @@ export class UserService{
         return this._http.post(this.url + 'login',params,{headers:headers})
                             .pipe(map(res =>res.json())); 
     }
+
+    register(user_to_register){
+        let params = JSON.stringify(user_to_register)
+        let headers = new Headers({'Content-Type': 'application/json'});
+
+        return this._http.post(this.url + 'register',params,{headers:headers})
+                            .pipe(map(res =>res.json())); 
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        if (identity !="udenfined"){
+            this.identity = identity;
+        }else{
+        this.identity = null;
+        }
+        return this.identity;
+    }
+    
+    getToken(){
+        let token = localStorage.getItem('token');
+
+        if(token!= "udenfinded"){
+            this.token = token;
+        }else{
+            this.token = null;
+        }
+        return this.token
+    }
+
 }
