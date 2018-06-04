84b0cc4dc829c2374feed2e1a04247cc184f7d91
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-06-04 19:02:26 -0400

    artist-list.component

---
+++ client/src/app/components/artist-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit } from '@angular/core';
+import{UserService} from '../services/user.service';
+import{Router,ActivatedRoute,Params} from '@angular/router';
+
+import {Artist} from '../models/artist';
+import{GLOBAL} from '../services/global';
+
+@Component({
+    selector:'artist-list',
+    templateUrl: '../Views/artisi-list.html',
+    providers: [UserService]
+})
+
+export class ArtistListComponent implements OnInit{
+    public titulo:string;
+    public artist: Artist[];
+    public identity;
+    public token;
+    public url: string;
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router,
+        private _userservice: UserService
+    ){
+        this.titulo = 'Artistas';
+        this.identity = this._userservice.getToken();
+        this.url = GLOBAL.url;
+    }
+
+    ngOnInit(){
+        console.log ('artist-list.component.ts cargado');
+
+        // Conseguir el listado de artistas
+    }
+}
