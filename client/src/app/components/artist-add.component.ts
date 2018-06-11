9013b38d18e98f520b372cb69684b80963824d18
Rodrigo Manieu Zumaran <rmanieu@fonasa.cl>
2018-06-11 17:12:54 -0400

    Clase 81 y 82

--- client/src/app/components/artist-add.component.ts
+++ client/src/app/components/artist-add.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit } from '@angular/core';
 import {UserService} from '../services/user.service';
+import {ArtistService} from '../services/artist.service';
 import {Router, ActivatedRoute, Params} from '@angular/router';
 
 import {Artist} from '../models/artist';
@@ -8,7 +9,7 @@ import {GLOBAL} from '../services/global';
 @Component({
     selector: 'artist-add',
     templateUrl: '../Views/artist-add.html',
-    providers: [UserService]
+    providers: [UserService, ArtistService]
 })
 
 export class ArtistAddComponent implements OnInit {
@@ -17,11 +18,13 @@ export class ArtistAddComponent implements OnInit {
     public identity;
     public token;
     public url: string;
+    public alertMessage;
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
-        private _userservice: UserService
+        private _userservice: UserService,
+        private _artistService: ArtistService
     ) {
         this.titulo = 'Crear Nuevo Artista';
         this.identity = this._userservice.getIdentity();
@@ -31,9 +34,30 @@ export class ArtistAddComponent implements OnInit {
     }
 
     ngOnInit() {
-        console.log ('artist-add.component.ts cargado');
+        // console.log ('artist-add.component.ts cargado');
+       // alert( this._artistService.addArtist());
     }
     onSubmit() {
-        console.log (this.artist);
+        // console.log (this.artist);
+        this._artistService.addArtist(this.token, this.artist).subscribe(
+            response => {
+                // respuesta de la BD
+                if (!response.artist) {
+                    this.alertMessage = 'Error en el servidor';
+                } else {
+                    this.alertMessage = 'El artista e creado Correctamente';
+                    this.artist = response.artist; 
+                   //  this._router.navigate(['/editar-artista'], response.artist._id);
+                }
+            },
+            error => {
+                const errorMessage = <any> error;
+                if (errorMessage != null) {
+                  const body = JSON.parse(error._body);
+                  this.alertMessage = body.message;
+                  console.log(error);
+                }
+              }
+        );
     }
 }
--- client/src/app/services/artist.service.ts
+++ client/src/app/services/artist.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {Http, Response, Headers} from '@angular/http';
 import {GLOBAL} from './global';
+import { map } from 'rxjs/operators';
+import { Artist } from '../models/artist';
 
 @Injectable()
 export class ArtistService {
@@ -11,8 +13,16 @@ export class ArtistService {
 
     constructor(private _http: Http) {
         this.url = GLOBAL.url;
-       
-        addArtist() {
-       return('Method not implemented.');
     }
+
+    addArtist(token, artist: Artist) {
+        let params = JSON.stringify(artist);
+        let headers = new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': token
+        });
+        return this._http.post(this.url + 'artist', params, {headers: headers})
+        .pipe(map(res => res.json()));
+    }
+
 }
